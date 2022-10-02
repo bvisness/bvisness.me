7a3fbffa6be16392f5e40e543f833767dfdcb136
Ben Visness <bvisness@gmail.com>
2022-10-02 15:03:51 -0500

    Add boggler

---
+++ updateapps.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+class dir:
+    def __init__(self, path):
+        self.path = path
+        self.initial_dir = os.getcwd()
+      
+    def __enter__(self):
+        os.chdir(self.path)
+  
+    def __exit__(self, type, value, traceback):
+        os.chdir(self.initial_dir)
+
+print('Updating git stuff...')
+subprocess.run(['git', 'submodule', 'init'])
+subprocess.run(['git', 'submodule', 'update', '--remote', '--recursive'])
+
+print('Updating boggler...')
+with dir('ext/boggler'):
+    subprocess.run(['python3', 'buildwords.py'])
+    subprocess.run(['python3', 'dist.py'])
