2fdb6c7d13d5c7bc3ac1010f745390d33d95246f
Ben Visness <bvisness@gmail.com>
2022-10-08 18:39:54 -0500

    Use the LRU cache for markdown (it helps a lot!)

--- pkg/markdown/markdown.go
+++ pkg/markdown/markdown.go
@@ -6,14 +6,24 @@ import (
 	"strings"
 	"unicode"
 
+	"github.com/bvisness/bvisness.me/pkg/lru"
 	gomarkdown "github.com/gomarkdown/markdown"
 	mdhtml "github.com/gomarkdown/markdown/html"
 )
 
+var markdownCache = lru.New[template.HTML](1000)
+
 var TemplateFuncs = template.FuncMap{
 	"markdown": func(md string) template.HTML {
 		md = Unindent(md)
-		return template.HTML(ToHTML(md))
+
+		if cachedHTML, ok := markdownCache.Get(md); ok {
+			return cachedHTML
+		} else {
+			html := template.HTML(ToHTML(md))
+			markdownCache.Store(md, html)
+			return html
+		}
 	},
 }
 
