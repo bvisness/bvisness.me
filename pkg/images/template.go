296fc59ac6ae9feaa87761bb2efe380bca8384d8
Ben Visness <bvisness@gmail.com>
2022-10-16 17:09:28 -0500

    Reorganize a little

--- pkg/images/template.go
+++ pkg/images/template.go
@@ -3,64 +3,12 @@ package images
 import (
 	"fmt"
 	"html/template"
-	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
 
 	"github.com/bvisness/bvisness.me/bhp"
 )
-
-func Middleware[T any](b bhp.Instance[T], r bhp.Request[T], w http.ResponseWriter, m bhp.MiddlewareData[T]) bool {
-	isImage := false
-	for _, format := range AllFormats {
-		if m.ContentType == format {
-			isImage = true
-		}
-	}
-
-	if !isImage {
-		return false
-	}
-
-	origScaleStr := r.R.URL.Query().Get("orig")
-	scaleStr := r.R.URL.Query().Get("scale")
-	if origScaleStr == "" || scaleStr == "" {
-		return false
-	}
-	origScale, err1 := strconv.Atoi(origScaleStr)
-	scale, err2 := strconv.Atoi(scaleStr)
-	if err1 != nil || err2 != nil {
-		return false
-	}
-	format := r.R.URL.Query().Get("fmt")
-
-	key := fmt.Sprintf("%s:orig(%d)", m.FilePath, origScale)
-	processed, err := imageCache.GetOrStore(key, func() (ProcessedImage, error) {
-		return ProcessImage(m.FilePath, origScale, ImageOptions{})
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	for _, variant := range processed.Variants {
-		scaleOk := variant.Scale == scale
-		var formatOk bool
-		if format == "" {
-			// Accept only the original content type
-			formatOk = variant.ContentType == processed.Source.ContentType
-		} else {
-			formatOk = variant.ContentType == format
-		}
-		if scaleOk && formatOk {
-			w.Header().Set("Content-Type", variant.ContentType)
-			w.Write(variant.Data)
-			return true
-		}
-	}
-
-	return false
-}
 
 func TemplateFuncs[T any](b bhp.Instance[T], r bhp.Request[T]) template.FuncMap {
 	return template.FuncMap{
