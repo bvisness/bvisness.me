416d14e47bc70cb793b4f79b6eed2c1fd744dace
Ben Visness <bvisness@gmail.com>
2022-10-16 19:10:56 -0500

    Convert banners to <picture>

--- pkg/images/template.go
+++ pkg/images/template.go
@@ -40,6 +40,7 @@ func TemplateFuncs[T any](b bhp.Instance[T], r bhp.Request[T]) template.FuncMap 
 			return strings.Join(candidates, ", "), nil
 		},
 
+		// WARNING! Chrome is big stupid!
 		"imageset": func(abspath string, scale int) (template.CSS, error) {
 			if scale < 1 {
 				scale = 1
@@ -69,12 +70,59 @@ func TemplateFuncs[T any](b bhp.Instance[T], r bhp.Request[T]) template.FuncMap 
 					}.Encode(),
 				)
 				options = append(options, fmt.Sprintf(
-					`url("%s") type("%s") %dx`,
-					url, variant.ContentType, variant.Scale,
+					`url("%s") %dx type("%s")`,
+					url, variant.Scale, variant.ContentType,
 				))
 			}
 
 			return template.CSS(strings.Join(options, ", ")), nil
+		},
+
+		"picturesource": func(abspath string, scale int) (template.HTML, error) {
+			if scale < 1 {
+				scale = 1
+			}
+
+			filepath, _, err := b.ResolveFile(abspath)
+			if err != nil {
+				return "", err
+			}
+
+			processed, err := imageCache.GetOrStore(cacheKey(filepath, scale), func() (ProcessedImage, error) {
+				return ProcessImage(filepath, scale, ImageOptions{})
+			})
+			if err != nil {
+				return "", err
+			}
+
+			optionsByType := make(map[string][]string)
+			for _, variant := range processed.Variants {
+				url := fmt.Sprintf(
+					"%s?%s",
+					bhp.AbsURL(r.R, abspath),
+					url.Values{
+						"orig":  {strconv.Itoa(processed.Source.Scale)},
+						"scale": {strconv.Itoa(variant.Scale)},
+						"fmt":   {variant.ContentType},
+					}.Encode(),
+				)
+				options := optionsByType[variant.ContentType]
+				optionsByType[variant.ContentType] = append(options, fmt.Sprintf(
+					`%s %dx`,
+					url, variant.Scale,
+				))
+			}
+
+			var b strings.Builder
+			for contentType, options := range optionsByType {
+				b.WriteString(fmt.Sprintf(
+					`<source srcset="%s" type="%s">`,
+					strings.Join(options, ", "), contentType,
+				))
+			}
+			b.WriteString(fmt.Sprintf(`<img src="%s">`, bhp.AbsURL(r.R, abspath)))
+
+			return template.HTML(b.String()), nil
 		},
 	}
 }
